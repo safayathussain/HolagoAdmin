e539b924a0a87eead2403d4885ad152b59c09232
safayat <safayatofficial1@gamil.com>
2024-10-24 12:41:38 +0600

    issue fix

--- app/auth/login/page.jsx
+++ app/auth/login/page.jsx
@@ -21,7 +21,6 @@ const Page = () => {
             phone_number: phone
         }
         const { data } = await FetchApi({ url: 'auth/api/tokenPair/', method: 'post', body: payload, isToast: true })
-        toast(data.otp || '')
         if (data.code === 200) {
             setStep(1)
         }
